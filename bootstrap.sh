1ce5c521af243232be3d50fe6a937595cc3952ef
tousaann <tousaann@gmail.com>
2019-06-22 16:32:58 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,6 @@ fi
 
 brew install git
 
-git clone --depth=1 -b ${BRANCH} https://github.com/touusann/macbootstrap.git ~/.macbootstrap
+git clone --depth=1 -b ${BRANCH} https://github.com/tousaann/macbootstrap.git ~/.macbootstrap
 cd ~/.macbootstrap
 bash install.sh
